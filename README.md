49dabdfa18654be23fb23bfb173690f35a40c54d
vienph12 <vien.pham@deltalabsjsc.com>
2025-10-17 14:42:47 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Build Onus Chain Contracts
+
+This project is used to build Onus Chain contracts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat ignition deploy ./ignition/modules/Lock.ts
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,58 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY ?? "";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.22",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    onusTestnet: {
+      url: 'https://rpc-testnet.onuschain.io',
+      accounts: [PRIVATE_KEY],
+      chainId: 1945,
+    },
+    onusMainnet: {
+      url: 'https://rpc.onuschain.io',
+      accounts: [PRIVATE_KEY],
+      chainId: 1975,
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      onusTestnet: "NONEED",
+      onusMainnet: "NONEED",
+    },
+    customChains: [
+      {
+        network: "onusTestnet",
+        chainId: 1945,
+        urls: {
+          apiURL: "https://explorer-testnet.onuschain.io/api",
+          browserURL: "https://explorer-testnet.onuschain.io",
+        },
+      },
+      {
+        network: "onusMainnet",
+        chainId: 1975,
+        urls: {
+          apiURL: "https://explorer.onuschain.io/api",
+          browserURL: "https://explorer.onuschain.io",
+        },
+      },
+    ],
+  },
+};
+
+export default config;
